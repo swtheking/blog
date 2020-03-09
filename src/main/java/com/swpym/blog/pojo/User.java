5371549d8d9426367887fdbca1d889ed33bca195
panym <526062864@qq.com>
2020-03-09 15:17:44 +0800

    1.整合了Jpa

--- src/main/java/com/swpym/blog/pojo/User.java
+++ src/main/java/com/swpym/blog/pojo/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 /**
  * @description:
  * @author: shaowei
@@ -12,8 +14,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Table(name = "user")
+@Entity
 public class User {
-    String Id;
+
+    @Id	//主键id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)//主键生成策略
+    @Column(name = "id")//数据库字段名
+    int Id;
+    @Column(name = "username")
     String username;
+    @Column(name = "password")
     String password;
 }
