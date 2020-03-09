49cc406622fe1221b7b74d9e8e588b836c8bb7bb
shaowei <1175233197@qq.com>
2020-03-09 11:39:27 +0800

    Initial commit

---
+++ src/test/java/com/swpym/blog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpym.blog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
