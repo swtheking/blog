56bd15d33b4d32cb1c99915e5449e09babaf47f1
shaowei <1175233197@qq.com>
2020-03-10 15:41:20 +0800

    加入緩存

--- src/test/java/com/swpym/blog/BlogApplicationTests.java
+++ src/test/java/com/swpym/blog/BlogApplicationTests.java
@@ -1,13 +1,23 @@
 package com.swpym.blog;
 
+import com.swpym.blog.pojo.User;
+import com.swpym.blog.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootTest
+@EnableCaching
 class BlogApplicationTests {
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	void contextLoads() {
+		User sw = userService.findAccountInfoByUsername("sw");
+		System.out.println(sw);
 	}
 
 }
